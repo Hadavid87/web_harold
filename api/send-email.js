d2db82653247809a96aaf5afba48ce4c271f29f1
Proyectos-Nexatech <proyectos@nexatech.com.co>
2026-04-07 08:22:22 -0500

    Update: change Resend account and destination email

--- api/send-email.js
+++ api/send-email.js
@@ -13,7 +13,7 @@ export default async function handler(req, res) {
   try {
     const data = await resend.emails.send({
       from: 'Portfolio Contact <onboarding@resend.dev>',
-      to: 'proyectos@nexatech.com.co', // Tu correo de destino
+      to: 'hadavid36@gmail.com', // Nueva cuenta: hadavid36
       subject: `Nuevo mensaje de ${name}`,
       html: `
         <h3>Nuevo mensaje de contacto</h3>
